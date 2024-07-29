a2a7be3cd6ec9b66ea71af592cf1328cb4c0ab78
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2024-07-29 19:05:23 +0530

    added update api

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,25 +1,32 @@
-import { useCreateRestaurant, useGetRestaurant } from "@/api/RestaurantApi";
+import {
+  useCreateRestaurant,
+  useGetRestaurant,
+  useUpdateRestaurant,
+} from "@/api/RestaurantApi";
 import ManageRestaurantForm from "@/components/restaurant/ManageRestaurant";
 
 const ManageRestaurantPage = () => {
-  const { isLoading, createRestaurant } = useCreateRestaurant();
-  const { restaurant } = useGetRestaurant();
+  const { isLoading: isCreateLoading, createRestaurant } =
+    useCreateRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateRestaurant();
+  const { restaurant, isLoading } = useGetRestaurant();
+
+  const isEditing = !!restaurant;
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <ManageRestaurantForm
-        onSave={(formData: FormData) => {
-          console.log(formData);
-          createRestaurant(formData);
-        }}
-        isLoading={isLoading}
-        restaurant={restaurant}
-      />
-    </div>
+    <ManageRestaurantForm
+      onSave={(formData: FormData) => {
+        if (isEditing) updateRestaurant(formData);
+        else createRestaurant(formData);
+      }}
+      isLoading={isCreateLoading || isUpdateLoading}
+      restaurant={restaurant}
+    />
   );
 };
 
